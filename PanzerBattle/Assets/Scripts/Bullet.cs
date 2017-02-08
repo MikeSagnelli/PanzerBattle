1722a6aa3dd92c4823b44ed05b43e4d2cbc31a2b
MikeSagnelli <ing.sagnelli@gmail.com>
2017-02-07 19:14:24 -0600

    Started developing project

---
+++ PanzerBattle/Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+		//method to retrieve a reference
+		rb = GetComponent<Rigidbody> ();
+
+		//if you don't have a component of that type (Rigidbody per se)
+		//a null will be returned
+
+		rb.AddRelativeForce (0, 10, 0, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ PanzerBattle/Assets/Scripts/Cannon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour {
+
+	//we need something to clone
+	public GameObject go;
+
+	private Transform parent;
+
+	private float oldJ;
+
+	// Use this for initialization
+	void Start () {
+
+		parent = transform.parent;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float h = Input.GetAxis ("Horizontal");
+		parent.transform.Translate(0, h * Time.deltaTime, 0);
+
+		float v = Input.GetAxis ("Vertical");
+		parent.Rotate (0, 0, v * Time.deltaTime * -20);
+
+		float j = Input.GetAxis ("Jump");
+
+	}
+}
